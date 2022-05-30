8ed5d374c50381ad59a7ff067678bde3927ff863
sajiburrahman <mrahman172152@bscse.uiu.ac.bd>
2022-05-30 17:22:23 +0600

    Change

--- E_health_consultation/settings.py
+++ E_health_consultation/settings.py
@@ -121,3 +121,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
